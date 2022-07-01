b26e4fdf69842319fec9db32ef1a6ed4c5752057
Ebuka Ejiofor <ebuskelele@gmail.com>
2022-07-01 08:18:55 +0100

    Add files

---
+++ main.js
@@ -0,0 +1,5 @@
+// User endpoint -https://api.ipregistry.co/?key=YOUR_API_KEY or http://ipwho.is/
+// User input endpoint - https://api.ipregistry.co/66.165.2.7?key=YOUR_API_KEY or http://ipwho.is/[IP address]
+
+// API_KEY - 7hnlideejegux5su
+
