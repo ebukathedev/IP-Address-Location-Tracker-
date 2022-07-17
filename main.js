4f7c10f11e7c817fd6ef7d7935466976c2d76e75
Ebuka Ejiofor <ebuskelele@gmail.com>
2022-07-17 03:32:40 +0100

    Update layout style

--- main.js
+++ main.js
@@ -16,11 +16,14 @@ const region = document.querySelector("#region");
 const city = document.querySelector("#city");
 const isp = document.querySelector("#isp");
 const skeletons = document.querySelectorAll(".value");
+const details = document.querySelector("#details");
+const errorMsg = document.querySelector("#error-msg");
+let userIP;
 
 // Remove the skeleton class
 const removeSKeletons = () => {
 	skeletons.forEach((skeleton) => {
-		skeleton.classList.remove("skeleton", "skeleton-text");
+		skeleton.classList.toggle("skeleton", "skeleton-text");
 	});
 };
 
@@ -51,33 +54,7 @@ const fillInValues = (
 fetch(url)
 	.then((res) => res.json())
 	.then((data) => {
-		removeSKeletons();
-		fillInValues(
-			data.ip,
-			data.type,
-			data.continent,
-			data.country,
-			data.capital,
-			data.region,
-			data.city,
-			data.connection.isp
-		);
-		console.log(data);
-	});
-
-form.addEventListener("submit", (e) => {
-	e.preventDefault();
-	const input = document.querySelector("#input");
-	let inputValue = document.querySelector("#input").value;
-	console.log(inputValue);
-
-	let urlForIPAddressEntered = url + inputValue;
-	console.log(urlForIPAddressEntered);
-
-	fetch(urlForIPAddressEntered)
-		.then((res) => res.json())
-		.then((data) => {
-			console.log(data);
+		setTimeout(function () {
 			fillInValues(
 				data.ip,
 				data.type,
@@ -88,7 +65,55 @@ form.addEventListener("submit", (e) => {
 				data.city,
 				data.connection.isp
 			);
-		});
+		}, 2000);
+		removeSKeletons();
+		localStorage.setItem("userIP", data.ip);
+		console.log(data);
+	});
+
+userIP = localStorage.getItem("userIP");
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const input = document.querySelector("#input");
+	let inputValue = document.querySelector("#input").value;
+	console.log(inputValue);
+
+	let urlForIPAddressEntered = url + inputValue;
+	console.log(urlForIPAddressEntered);
+
+	if (userIP !== inputValue) {
+		details.innerText = "Details for the IP entered";
+	} else details.innerText = "Details for your IP";
+	// let arr = Array(9).fill("up");
+	// fillInValues(...arr);
+	// removeSKeletons();
+
+	// fetch(urlForIPAddressEntered)
+	// 	.then((res) => res.json())
+	// 	.then((data) => {
+	// 		console.log(data);
+	// 		removeSKeletons();
+	// 		setTimeout(function () {
+	// 			fillInValues(
+	// 				data.ip,
+	// 				data.type,
+	// 				data.continent,
+	// 				data.country,
+	// 				data.capital,
+	// 				data.region,
+	// 				data.city,
+	// 				data.connection.isp
+	// 			);
+	// 		});
+	// 	})
+	// 	.catch((error) => {
+	// 		console.log(inputValue + "is a private IP address");
+	// 		errorMsg.innerText = `"${inputValue} is either not valid or it is a private IP address, try a different address :)`;
+	// 		errorMsg.classList.add("disappear");
+	// 		errorMsg.innerText = "";
+	// 		errorMsg.classList.remove("disappear");
+	// 	});
 
 	input.value = "";
 });
